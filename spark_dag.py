543d0c9561220780be85e9128ec3b0682ce95142
andonokar <anderson.lima@domrock.ai>
2023-10-26 10:30:03 -0300

    dag testing upload

---
+++ spark_dag.py
@@ -0,0 +1,23 @@
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+
+from airflow import DAG
+from datetime import datetime
+
+default_args = {
+    'owner': 'anderson',
+    'start_date': datetime(2023, 1, 1),
+    'depends_on_past': False,
+    'retries': 0,
+}
+
+with DAG(
+    'my_spark_dag',
+    default_args=default_args,
+    schedule_interval=None,  # Set to None if you don't want the DAG to be scheduled
+) as dag:
+    spark_task = SparkKubernetesOperator(
+        task_id='test_spark',
+        application_file='spark.yaml',
+        namespace="spark-operator",
+        name='your_spark_job'
+    )
