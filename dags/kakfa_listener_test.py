afbfea82c15d3552f9ef04db7c5debb087165454
andonokar <anderson.lima@domrock.ai>
2023-11-03 15:42:22 -0300

    TESTING

--- dags/kakfa_listener_test.py
+++ dags/kakfa_listener_test.py
@@ -16,7 +16,9 @@ with DAG(
     'my_kafka_dagv2',
     default_args=default_args,
     schedule_interval="@continuous",  # Set to None if you don't want the DAG to be scheduled
-    max_active_runs=1
+    max_active_runs=1,
+    catchup=False,
+    is_paused_upon_creation=False
 ) as dag:
     def await_function(message):
         print(message)
@@ -25,18 +27,18 @@ with DAG(
         except Exception as err:
             print(type(err).__name__ + ': ' + str(err))
         else:
-            val.get("csn")
+            val.get("table_name")
             return val
 
     def wait_for_event(message, **context):
         TriggerDagRunOperator(
             trigger_dag_id=message,
-            task_id=f"triggered_downstream_dag_{uuid.uuid4()}",
+            task_id=f"triggered_downstream_dag_{message}",
         ).execute(context)
 
     kafka_task = AwaitMessageTriggerFunctionSensor(
         task_id='test_kafka',
-        topics=['teste'],
+        topics=['csn'],
         apply_function="kakfa_listener_test.await_function",
         event_triggered_function=wait_for_event
     )
--- dags/spark_dag.py
+++ dags/spark_dag.py
@@ -11,13 +11,49 @@ default_args = {
 }
 
 with DAG(
-    'my_spark_dag',
+    'csn_demanda',
     default_args=default_args,
     schedule_interval=None,  # Set to None if you don't want the DAG to be scheduled
+    max_active_runs=1,
+    catchup=False,
+    is_paused_upon_creation=False
 ) as dag:
-    spark_task = SparkKubernetesOperator(
-        task_id='test_spark',
-        application_file='spark.yaml',
+    demanda_bronze = SparkKubernetesOperator(
+        task_id='demanda_bronze',
+        application_file='demanda_bronze.yaml',
         namespace="spark-operator",
         watch=True
     )
+    demanda_silver = SparkKubernetesOperator(
+        task_id='demanda_silver',
+        application_file='demanda_silver.yaml',
+        namespace="spark-operator",
+        watch=True,
+        trigger_rule="none_skipped"
+    )
+    demanda_bronze >> demanda_silver
+
+with DAG(
+    'csn_carteira',
+    default_args=default_args,
+    schedule_interval=None,  # Set to None if you don't want the DAG to be scheduled
+    max_active_runs=1,
+    catchup=False,
+    is_paused_upon_creation=False
+) as dag2:
+    carteira_bronze = SparkKubernetesOperator(
+        task_id='carteira_bronze',
+        application_file='carteira_bronze.yaml',
+        namespace="spark-operator",
+        watch=True
+    )
+    carteira_silver = SparkKubernetesOperator(
+        task_id='carteira_silver',
+        application_file='carteira_silver.yaml',
+        namespace="spark-operator",
+        watch=True,
+        trigger_rule="none_skipped"
+    )
+    carteira_bronze >> carteira_silver
+
+
