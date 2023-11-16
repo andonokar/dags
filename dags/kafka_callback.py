672ab9f60d1dc273a0f36118f5f0ad8bfcb3e73a
andonokar <anderson.lima@domrock.ai>
2023-11-16 17:46:34 -0300

    TESTING

--- dags/kafka_callback.py
+++ dags/kafka_callback.py
@@ -7,7 +7,7 @@ def producer_function(text):
 
 
 def produce_to_kafka(context):
-    output = json.dumps(context.as_dict())
+    output = json.dumps(context.items())
 
     producer = ProduceToTopicOperator(
         kafka_config_id="kafka_producer_1",
@@ -16,4 +16,4 @@ def produce_to_kafka(context):
         producer_function=producer_function,
         producer_function_args=output
     )
-    producer.execute(context.as_dict())
+    producer.execute(context.items())
