fb552bd1d37825002896f5ff206adcef3541a572
andonokar <anderson.lima@domrock.ai>
2023-11-23 17:45:34 -0300

    TESTING

--- dags/kafka_callback.py
+++ dags/kafka_callback.py
@@ -21,9 +21,9 @@ def produce_to_kafka(context):
         "log_url": str(ti.log_url)
     }
 
-    log_url = f'http://airflow-webserver:8080/api/v1/dags/{ti.dag_id}/dagRuns/{ti.run_id}/taskInstances/{ti.task_id}/logs/{ti.try_number}'
-    params = {"full_content": True}
-    response = requests.get(log_url, params=params, auth=("admin", "admin"))
+    log_url = f'http://airflow-webserver:8080/api/v1/dags/{ti.dag_id}/dagRuns/{ti.run_id}/taskInstances/{ti.task_id}/logs/{ti.try_number}?full_content=true'
+    # params = {"full_content": True}
+    response = requests.get(log_url, auth=("admin", "admin"))
     # if response.status_code == 200:
     logs = response.content.decode('utf-8')
     output['logs'] = logs
